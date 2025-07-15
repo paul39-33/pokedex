933eaf31c4ff3511839a0e3680536eb89535baff
paulus <paulusojak@gmail.com>
2025-07-15 13:11:04 +0700

    Create basic main and gitignore

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/paul39-33/pokedex
+
+go 1.24.4
---
+++ main.go
@@ -0,0 +1,13 @@
+package main
+import "fmt"
+
+func cleanInput(text string) []string {
+	var blank []string{}
+	return blank
+}
+
+func main(){
+	fmt.Println("Hello, World!")
+}
+
+
